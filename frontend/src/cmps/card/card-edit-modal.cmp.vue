5599abd3f6d3b766d33f53904fd2c80f21cd86d6
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-07-16 16:55:03 +0300

    edit modal base

---
+++ frontend/src/cmps/card/card-edit-modal.cmp.vue
@@ -0,0 +1,26 @@
+<template>
+	<section class="card-edit-modal">
+		<button onclick="close">X</button>
+		<slot></slot>
+	</section>
+	<!-- <modal v-if="showModal" @close="showModal = false"> -->
+</template>
+<script>
+export default {
+	props: [],
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {
+		close() {
+			this.$emit("closeModal");
+		}
+	},
+	created() {},
+	components: {}
+};
+</script>
+
+<style>
+</style>
