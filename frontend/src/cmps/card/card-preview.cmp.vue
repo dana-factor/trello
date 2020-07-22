b9fac89856299c133e9db5d7eb6e836243e3c2f4
Samantha Kofler <samantha_kofler@outlook.com>
2020-07-22 20:08:40 +0300

    members card preview

--- frontend/src/cmps/card/card-preview.cmp.vue
+++ frontend/src/cmps/card/card-preview.cmp.vue
@@ -37,12 +37,22 @@
 			<p v-if="card.attachments.length">
 				<i class="el-icon-paperclip"></i>
 			</p>
+			<!-- <p > -->
+				<ul v-if="card.members.length" class="members-preview">
+							<li
+								v-for="member in card.members"
+								:key="member._id"
+								:title="member.fullName"
+							><avatar :src="member.imgUrl" :username="member.fullName" :size="25" /></li>
+						</ul>
+			<!-- </p> -->
 		</router-link>
 	</section>
 </template>
 
 <script>
 import { utilService } from '../../services/util.service.js';
+import Avatar from 'vue-avatar';
 
 export default {
 	props: ['card', 'boardLabels'],
@@ -93,7 +103,9 @@ export default {
 		}
 	},
 	created() { },
-	components: {}
+	components: {
+		Avatar
+	}
 };
 </script>
 
