78b14c032dd880cc75e948fe031706e58bee10d0
Samantha Kofler <samantha_kofler@outlook.com>
2020-07-23 19:06:36 +0300

    charts

---
+++ frontend/src/cmps/board/chart-members.cmp.vue
@@ -0,0 +1,37 @@
+
+<script>
+
+import VueCharts from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
+Chart.defaults.global.legend.display = false;
+export default {
+  extends: Bar,
+  props: ['labels', 'data'],
+  mounted () {
+      console.log(this.labels, this.data)
+    this.renderChart({
+      labels: this.labels,
+      datasets: [
+        {
+          label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           },
+          backgroundColor: ['#6B4C9A', '#948B3D', '#396AB1', '#DA7C30', '#3E9651', '#CC2529', '#535154', '#922428'],
+          data: this.data
+        }
+      ]
+   
+    },
+      {
+        scales: {
+        yAxes: [{ticks: {min: 0}}]
+    }
+    })
+  }
+}
+
+
+</script>
+
+<style>
+</style>
---
+++ frontend/src/cmps/board/chart-phases.cmp.vue
@@ -0,0 +1,43 @@
+
+
+<script>
+import VueCharts from 'vue-chartjs'
+import { Pie } from 'vue-chartjs'
+
+export default {
+    extends: Pie,
+    props:['labels', 'data'],
+    data(){
+        return{
+
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+    created(){
+    },
+    mounted(){
+    this.renderChart({
+      labels: this.labels,
+      datasets: [
+        {
+          label: 'Cards per Phase',
+          backgroundColor: ['#6B4C9A', '#948B3D', '#396AB1', '#DA7C30', '#3E9651', '#CC2529', '#535154', '#922428'],
+          data: this.data
+        }
+      ]
+    })
+
+    }
+
+}
+</script>
+
+<style>
+
+
+</style>
