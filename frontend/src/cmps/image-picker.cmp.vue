bd16d15d9d7848f298b81508c21d34a61a0b118b
Samantha Kofler <samantha_kofler@outlook.com>
2020-07-25 19:07:08 +0300

    bg images

--- frontend/src/cmps/image-picker.cmp.vue
+++ frontend/src/cmps/image-picker.cmp.vue
@@ -18,16 +18,18 @@ export default {
 			imgUrls: [
                 "http://res.cloudinary.com/ddh9unyrw/image/upload/v1595247424/mqzclgmqseuvpfyg98j1.jpg",
                 'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595257632/jwx3pgn88cupsgb3vm2h.jpg',
-                'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595257679/cdvgktjmmsoznwssjzso.jpg',
-                'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595257734/bbarpby4naz9ylnffwqn.jpg',
+                'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595692812/c2nvonx0vniezgg7sv0s.jpg',
+                'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595692877/ankqowegp6237hijoiey.jpg',
                 'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595257759/vexglerwamwj6hmilkjc.jpg',
 				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595257780/qyse9g8vtia73btrliqh.jpg',
 				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595317869/in3rirgiuy9056gxunam.jpg',
-				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595317909/fxkp583iqxcbvy8x02ed.jpg',
+				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595692898/xk0u73cqvsaiehceablw.jpg',
+				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595693125/q1ipc1rmoytpg8x8pgvo.jpg',
 				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595317933/akw7galczskf0pt1ya1p.jpg',
-				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595317959/yqnzpcwbddqrnrzt9m1t.jpg',
-				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595317981/fxjuqwiu1knhcali6g4f.jpg',
-				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595318188/ysas5hxlpkxfu6ocrf7p.jpg'
+				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595692927/reagthnikpshsdcrtcfw.jpg',
+				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595692960/rrhvawsjtr9x8ccbse2m.jpg',
+				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595692983/aty5zrzh3oihgfasdjxj.jpg',
+				'http://res.cloudinary.com/ddh9unyrw/image/upload/v1595693031/zjtea7n1iky4wjtcovzf.jpg'
 			]
 		};
 	},
