8284f3a236edaa575ea2ce689948c09338ea4581
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-07-27 15:39:27 +0300

    activity and socket small fix

--- frontend/src/store/board.store.js
+++ frontend/src/store/board.store.js
@@ -84,7 +84,7 @@ export const boardStore = {
 			try {
 				const type = board._id ? 'updateBoard' : 'addBoard';
 				const savedBoard = await boardService.save(board, activity);
-				this._vm.$socket.client.emit('boardChanged', board);
+				this._vm.$socket.client.emit('boardChanged', savedBoard);
 				commit({ type, board: savedBoard });
 				return savedBoard;
 			} catch (err) {
