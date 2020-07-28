7dd096983084be6b275415acc545bb53a62a986c
Opal Mizrahi <opal.mizrahi2@gmail.com>
2020-07-28 11:02:57 +0300

    update homepage texts

--- frontend/src/views/homepage.cmp.vue
+++ frontend/src/views/homepage.cmp.vue
@@ -1,28 +1,27 @@
 <template>
-	<section class="homepage">
-
-		<div class="hero-container">
-			<img src="../../src/assets/img/hero.png" />
-			<router-link to="/board/5f1aefd0f30ae5001757bd46">
-				<h1>try trellA now</h1>
-			</router-link>
-		</div>
-		<div class="notes-container">
-			<div class="yellow-note note">
-				<h2>Drag n Drop</h2>
-				<p>Try our Drag n Drop technology to easily move & update your tasks with your mouse or finger.</p>
-			</div>
-			<div class="blue-note note">
-				<h2>Make it yours</h2>
-				<p>Make it feel like home for you and your team. Customize the board's appearance to fit the team's flow.</p>
-			</div>
-			<div class="green-note note">
-				<h2>Real-time Updating</h2>
-				<p>Be always up-to-date. You and your team can enjoy a live workspace that shows you also the most recent changes.</p>
-			</div>
-		</div>
-		<app-footer/>
-	</section>
+  <section class="homepage">
+    <div class="hero-container">
+      <img src="../../src/assets/img/hero.png" />
+      <router-link to="/board/5f1aefd0f30ae5001757bd46">
+        <h1>try trellA now</h1>
+      </router-link>
+    </div>
+    <div class="notes-container">
+      <div class="yellow-note note">
+        <h2>Real-time Updating</h2>
+        <p>You and your team can enjoy a live workspace that syncs in real-time and allows working simultaneously.</p>
+      </div>
+      <div class="blue-note note">
+        <h2>Control your privacy</h2>
+        <p>Set lists as private to make them invisible to others and keep track of personal tasks on your shared boards.</p>
+      </div>
+      <div class="green-note note">
+        <h2>Instant Sharing</h2>
+        <p>Quickly share tasks on Whatsapp, Facebook and Email with your team and even new members.</p>
+      </div>
+    </div>
+    <app-footer />
+  </section>
 </template>
 
 <script>
@@ -41,13 +40,13 @@ export default {
 	methods: {
 	},
 	created() {
-        this.$store.dispatch({type: 'loadBoards'})
-        .then(() => {
-             this.boards = this.$store.getters.boards
-             })
+		this.$store.dispatch({ type: 'loadBoards' })
+			.then(() => {
+				this.boards = this.$store.getters.boards
+			})
 
-    },
-	mounted() {},
+	},
+	mounted() { },
 	watch: {},
 	components: {
 		boardList,
